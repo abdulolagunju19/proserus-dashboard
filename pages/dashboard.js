466911123a3e9ac5202c396636188537dbef2c44
Abdul-Samad Olagunju <abdulolagunju19@gmail.com>
2023-05-29 16:21:21 -0600

    add financial modeling prep api call

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,21 +1,46 @@
-import { Flex, Heading, Button } from '@chakra-ui/react';
+import { Flex, Heading, Box, Text } from '@chakra-ui/react';
 
 import DashboardContainer from '@/components/DashboardContainer';
 import Simple from '@/components/Navbar';
 import NextLink from 'next/link'
 
-const DashboardIndex = () => {
+const DashboardIndex = ( { data } ) => {
+    console.log(data)
     return(
         <DashboardContainer>
-            <Flex
-                flexDir="column"
-                maxH="100vh"
-                maxW="150vh"
-            >
-                <Heading>Financial Report</Heading>
-            </Flex>
+            <Heading pb={2}>Financial Report</Heading>
+            <div>
+                {data.map((datum) => (
+                <Box p={5} m={3} borderWidth='1px' borderRadius='lg' overflow='hidden'>
+                    <Heading pb={2}>{datum.symbol}</Heading>
+                    <Heading size="h3">Calendar Year: {datum.calendarYear}</Heading>
+                    <Text>Revenue: ${datum.revenue}</Text>
+                    <Text>Operating Expenses: ${datum.operatingExpenses}</Text>
+                </Box>
+                ))}
+            </div>
         </DashboardContainer>
     )
+}
+
+export async function getServerSideProps(){
+    const res = await fetch(`https://financialmodelingprep.com/api/v3/income-statement/AAPL?limit=120&apikey=${process.env.FINANCIAL_MODELING_PREP_KEY}`);
+    const data = await res.json();
+
+    if (!data) {
+        return {
+          redirect: {
+            destination: '/',
+            permanent: false,
+          },
+        }
+    }
+
+    return{
+        props:{
+            data
+        }
+    }
 }
 
 export default DashboardIndex;
